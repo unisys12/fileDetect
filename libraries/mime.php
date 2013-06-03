a99131938a1a24ae0985c2842653e429c9d10ecb
Phillip Jackson <unisys12@gmail.com>
2013-06-02 19:52:14 -0500

    Updated static functions
    Created static functions for each of the global $_FILES contents
    supported

--- libraries/mime.php
+++ libraries/mime.php
@@ -3,16 +3,57 @@ class File {
 
 	public $output;
 	public $file;
-/*
-	public function __construct($file)
+
+	public function __construct()
 	{
-		$file = $_FILES['file']['name'];
+
 	}
-*/
+
 
 	public static function getFile($file)
 	{
 
+		//$file = $_FILES['file'];
+		return $file;
+
+	}
+
+	public static function getFileName($file)
+	{
+
+		$file = File::getFile($file['name']);
+		return $file;
+
+	}
+
+	public static function getFileType($file)
+	{
+
+		$file = File::getFile($file['type']);
+		return $file;
+
+	}
+
+	public static function getFileSize($file)
+	{
+
+		$file = File::getFile($file['size']);
+		return $file;
+
+	}
+
+	public static function getFileTemp($file)
+	{
+
+		$file = File::getFile($file['tmp_name']);
+		return $file;
+
+	}
+
+	public static function getFileError($file)
+	{
+
+		$file = File::getFile($file['error']);
 		return $file;
 
 	}
