7106f6158202d2c690f2cc7fffff795d9a74b1c5
Phillip Jackson <unisys12@gmail.com>
2013-06-07 18:14:25 -0500

    only show MIME type of file

--- partials/file_upload.php
+++ partials/file_upload.php
@@ -14,9 +14,10 @@
 	{
 		$file = $_FILES['file'];
 
-		echo '<li>' . File::getFileName($file) . '</li>';
+		// echo '<li>' . File::getFileName($file) . '</li>';
 		echo '<li>' . File::getFileType($file) . '</li>';
-		echo '<li>' . File::getFileSize($file) . 'kb </li>';
+		// echo '<li>' . File::getFileSize($file) . 'kb </li>';
+		echo var_dump($_FILES);
 	}
 	else
 	{
